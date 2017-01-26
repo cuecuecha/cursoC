7bd71365756ddc483e3eb47757120d859982f1d6
cuecuecha <root@bases.jorge>
2017-01-25 18:17:19 -0600

    practica3

--- practica3/fin_mundo.c
+++ practica3/fin_mundo.c
@@ -3,11 +3,11 @@ Programa que muestra la fecha maxima que soporta time_t, desde el origen en 1-01
 */
 #include<stdio.h>
 #include<time.h>
-
+#include<limits.h>
 int main()
 {
 	time_t t = INT_MAX; //guardo el máximo valor que tiene un long int
-	time_t* pt1=&t1;  //guarda la direccion de memoria de t1
+	time_t* pt1=&t;  //guarda la direccion de memoria de t1
 	char* sTptr=ctime(pt1); //ctime recibe los segundos que seran transformandos a una fecha
 	printf("El fin del mundo es: %s ",sTptr);//imprimo la fecha
 	return 0;
