3a44ed19e1a22b7a3547291d9e8c4171c19f5501
cuecuecha <root@bases.jorge>
2017-01-26 15:48:36 -0600

    practica14 y dia4

---
+++ practica14/archivo.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main(int argc, char *argv[])
+{
+
+#if 0
+	if(argc<2)
+	{
+		perror("Se necesita un argumento\n");
+		return(1111);
+	}
+#endif
+
+	int fd; //Declaracion de un descriptor de archivos.
+	char *archivo="/dev/random\0";
+	int numero;
+	fd=open(archivo,O_RDONLY);
+	
+	/*
+	open --> -1 si no se puede abrir
+	append --> escribe al final, no se sobreescribe
+	S_IRUSR --> set- read user(RU)
+	*/
+	
+	if(fd==-1)
+		fprintf(stderr,"No se pudo abrir o crear el archivo");
+	printf("Descriptor de archivo: %d\n",fd);
+
+//#if 0
+	
+	if(read(fd,numero,sizeof(int))==-1)
+		printf("%u\n",numero);
+//#endif
+
+	if(close(fd)==-1)
+		fprintf(stderr,"No se pudo cerrar el descriptor de archivo");
+}
